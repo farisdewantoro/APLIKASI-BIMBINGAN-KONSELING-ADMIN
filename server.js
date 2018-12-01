67f915bf20a7e4ae652b6431a83c32acbf76ac4b
Faris Dewantoro <farisd117@gmail.com>
2018-12-01 21:29:55 +0700

    as

--- server.js
+++ server.js
@@ -26,9 +26,10 @@ mongoose
         console.log(err);
     });
 app.use((req, res, next) => { 
-    res.header("Access-Control-Allow-Origin", "*"); 
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-     next();  });
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); 
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+    next(); });
 // Passport middleware
 app.use(passport.initialize());
 // Passport Config
